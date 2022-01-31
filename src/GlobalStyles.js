de8e9b654ac80c6316ad606e08926115e13e5eb0
Ogunyomade Yemi! <yhemyogunyomade@gmail.com>
2022-01-31 20:55:43 +0100

    created neccessary files and install required dependencies to kickstart the project

---
+++ src/GlobalStyles.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+    body{
+        background-color: chocolate;
+        margin: 0;
+        padding: 0;
+        
+
+    }
+    a{
+        text-decoration: none;
+        color: #000;
+    };
+
+    *{
+        box-sizing: border-box;
+    }
+
+
+`
